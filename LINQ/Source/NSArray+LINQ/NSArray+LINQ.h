ad84ee79971f58af2f4bc389728515a28982ad4f
Michal Konturek <michal.konturek@gmail.com>
2013-06-22 17:45:20 +0100

    Implemented NSArray+LINQ_Generation and tests.

---
+++ LINQ/Source/NSArray+LINQ/NSArray+LINQ.h
@@ -0,0 +1,9 @@
+//
+//  NSArray+LINQ.h
+//  LINQ
+//
+//  Created by Michal Konturek on 22/06/2013.
+//  Copyright (c) 2013 Michal Konturek. All rights reserved.
+//
+
+#import "NSArray+LINQ_Generation.h"
